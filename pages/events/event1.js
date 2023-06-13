59cf4824ba976caadbe59597db2577501a8fd97d
PKILB <paytonboone30@gmail.com>
2023-06-12 18:34:22 -0600

    beginning of dynamic page is created

--- pages/events/event1.js
+++ pages/events/event1.js
@@ -1,7 +1,5 @@
 const Page = () => {
-    return (
-        <h1> Event Page </h1>
-    )
-}
+    return <h1> Event Page </h1>
+};
 
 export default Page;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,22 @@
-const Page = () => {
-    return (
-        <h1> Events Page </h1>
-    )
+const EventsPage = () => {
+    return <div>
+        <h1>Events Page</h1>
+        <div>
+            <a href=''>
+                <img />
+                <h2>Events in London</h2>
+            </a>
+            <a href=''>
+                <img />
+                <h2>Events in San Francisco</h2>
+            </a>
+            <a href=''>
+                <img />
+                <h2>Events in Barcelona</h2>
+            </a>
+        </div>
+    </div>
 }
 
-export default Page;
+export default EventsPage;
+
